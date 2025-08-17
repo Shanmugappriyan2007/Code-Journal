379d819af9955bb76ad0a266a349d32d296c59b5
Shanmugapriyan R <shanmugapriyan349@gmail.com>
2025-08-18 05:26:45 +0530

    Create Day 7 - Switch case.java
    This is a beginner-friendly Java program that demonstrates the use of a switch statement. The program takes input from the user and performs basic arithmetic operations such as: Addition, Subtraction, Multiplication and Division.

---
+++ Day 7 - Switch case.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Choose an operation: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        int choice = sc.nextInt();
+      
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+        sc.close();
+    }
+}
