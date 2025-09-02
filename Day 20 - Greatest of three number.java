87bc68a3a2a56e7d68bb6a019ade7c2e21f5d524
Shanmugapriyan R <shanmugapriyan349@gmail.com>
2025-09-02 06:07:57 +0530

    Create Day 20 - Greatest of three number.java
    This Java program finds the greatest (largest) number among three numbers entered by the user. It compares the three numbers using conditional statements (if-else). The largest number is then displayed as output.

---
+++ Day 20 - Greatest of three number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class GreatestOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+
+        int greatest;
+        if (a >= b && a >= c) {
+            greatest = a;
+        } else if (b >= a && b >= c) {
+            greatest = b;
+        } else {
+            greatest = c;
+        }
+        System.out.println("The greatest number is: " + greatest);
+        scanner.close();
+    }
+}
