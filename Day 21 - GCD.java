a2841aaddee8a11d0a22c4c61ed04af37e200126
Shanmugapriyan R <shanmugapriyan349@gmail.com>
2025-09-03 06:21:00 +0530

    Create Day 21 - GCD.java
    This Java program calculates the Greatest Common Divisor (GCD), also known as the Highest Common Factor (HCF), of two numbers.

---
+++ Day 21 - GCD.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("GCD (Greatest Common Divisor) is: " + a);
+        scanner.close();
+    }
+}
